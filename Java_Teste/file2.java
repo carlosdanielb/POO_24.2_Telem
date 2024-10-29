3c19c3420b0af2215215f22e7c90b815a606c60c
carlosdanielb <164261112+carlosdanielb@users.noreply.github.com>
2024-10-29 20:13:41 -0300

    file2.java

---
+++ Java_Teste/file2.java
@@ -0,0 +1,17 @@
+package teste;
+
+public class Teste_01 {
+	
+public static void main(String[] args) {
+		String abu = "batata";
+		String aba = "asas";
+		System.out.println(abu);
+		System.out.println(aba);
+}
+void meuMetetodo() {
+	long x = 20; //Variável local
+	int i = (int) x;
+	System.out.println(x);
+}
+}
+//Referente á aula do dia 29/10/2024
