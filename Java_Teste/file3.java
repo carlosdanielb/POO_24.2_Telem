575d2897fc0dd9b10ca4c786f1e67433dc258513
carlosdanielb <164261112+carlosdanielb@users.noreply.github.com>
2024-10-30 19:41:22 -0300

    file3.java

---
+++ Java_Teste/file3.java
@@ -0,0 +1,23 @@
+package teste;
+
+public class Teste_01 {
+	
+public static void main(String[] args) {
+	
+		String nome = "bolo"; //Variável 1
+		String sobrenome = "de cake";
+		int a = 7;
+		String tipoAnimal = "gatos";
+		
+		System.out.printf("%s %s tem %d %s %n",nome,sobrenome,a,tipoAnimal); //Use printf neste modelo.
+		
+		String name = "Rodrigo"; //Variável 2
+		int age = 20; 
+		String year = "Anos"; 
+		float height = 1.70f;
+		
+		System.out.printf("%s tem %d %s %nE tem %f de altura",name,age, year, height); //Use printf neste modelo.
+}
+}
+/*Referente á aula do dia 30/10/2024
+  */
