15c717caac981ca536be6cf2f28ceeb5ee84c2c4
carlosdanielb <164261112+carlosdanielb@users.noreply.github.com>
2024-11-06 14:32:17 -0300

    atv1

---
+++ atividades/atv1/atv8.java
@@ -0,0 +1,17 @@
+public class TESTE_01 {
+    public static void main(String[] args) {
+        // Declara a variável contador com valor inicial de 5
+        int contador = 5;
+        
+        // Exibe o valor inicial
+        System.out.println("Valor inicial do contador: " + contador);
+        
+        // Usa o operador de incremento para aumentar o valor
+        contador++;
+        System.out.println("Valor após incremento: " + contador);
+        
+        // Usa o operador de decremento para diminuir o valor
+        contador--;
+        System.out.println("Valor após decremento: " + contador);
+    }
+}
